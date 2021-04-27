857c83ce435a24d24cff6ee2007ab8a967be4b36
Noah de Boers <63986217+AccedePro@users.noreply.github.com>
2021-04-28 03:09:13 +1000

    Add files via upload

---
+++ PruneEnd.bat
@@ -0,0 +1,16 @@
+"C:\Users\WinServ\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\WinServ\Documents\IsisCraft (Bungeecord)\PruneEnd.py"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PruneEnd.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+exception = ["r.-3.-3.mca","r.-3.-2.mca","r.-3.-1.mca","r.-3.0.mca","r.-3.1.mca","r.-3.2.mca","r.-2.-3.mca","r.-2.-2.mca","r.-2.-1.mca","r.-2.0.mca","r.-2.1.mca","r.-2.2.mca","r.-1.-3.mca","r.-1.-2.mca","r.-1.-1.mca","r.-1.0.mca","r.-1.1.mca","r.-1.2.mca","r.0.-3.mca","r.0.-2.mca","r.0.-1.mca","r.0.0.mca","r.0.1.mca","r.0.2.mca","r.1.-3.mca","r.1.-2.mca","r.1.-1.mca","r.1.0.mca","r.1.1.mca","r.1.2.mca","r.2.-3.mca","r.2.-2.mca","r.2.-1.mca","r.2.0.mca","r.2.1.mca","r.2.2.mca"]
+filelist = os.listdir('C:/Users/WinServ/Documents/IsisCraft (Bungeecord)/Servers/Main [1.16.5]/main_the_end/DIM1/region')
+found = []
+notfound = []
+
+for directories in filelist:
+    for value in exception:
+        if value in directories:
+            found.append(directories)
+
+
+for files in filelist:
+    if files in found:
+        print(files + " found.")
+    else:
+        files.replace("'","")
+        notfound.append(files)
+
+if len(notfound) == 0:
+    print("no files to delete...")
+    exit()
+else:
+    print("getting ready to delete files...")
+
+time.sleep(3)
+
+for deletefile in notfound:
+    print(deletefile + " deleted.")
+    os.remove('C:/Users/WinServ/Documents/IsisCraft (Bungeecord)/Servers/Main [1.16.5]/main_the_end/DIM1/region/' + deletefile)
+
+    
