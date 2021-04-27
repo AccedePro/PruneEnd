9b7ed0b5a2598efab4e2dc6a5422c29900a6199f
Noah de Boers <63986217+AccedePro@users.noreply.github.com>
2021-04-28 03:23:45 +1000

    Update PruneEnd.py

--- PruneEnd.py
+++ PruneEnd.py
@@ -3,6 +3,7 @@ import time
 
 exception = ["r.-3.-3.mca","r.-3.-2.mca","r.-3.-1.mca","r.-3.0.mca","r.-3.1.mca","r.-3.2.mca","r.-2.-3.mca","r.-2.-2.mca","r.-2.-1.mca","r.-2.0.mca","r.-2.1.mca","r.-2.2.mca","r.-1.-3.mca","r.-1.-2.mca","r.-1.-1.mca","r.-1.0.mca","r.-1.1.mca","r.-1.2.mca","r.0.-3.mca","r.0.-2.mca","r.0.-1.mca","r.0.0.mca","r.0.1.mca","r.0.2.mca","r.1.-3.mca","r.1.-2.mca","r.1.-1.mca","r.1.0.mca","r.1.1.mca","r.1.2.mca","r.2.-3.mca","r.2.-2.mca","r.2.-1.mca","r.2.0.mca","r.2.1.mca","r.2.2.mca"]
 filelist = os.listdir('C:/Users/WinServ/Documents/IsisCraft (Bungeecord)/Servers/Main [1.16.5]/main_the_end/DIM1/region')
+#this path does not require a final slash at the end
 found = []
 notfound = []
 
@@ -30,5 +31,6 @@ time.sleep(3)
 for deletefile in notfound:
     print(deletefile + " deleted.")
     os.remove('C:/Users/WinServ/Documents/IsisCraft (Bungeecord)/Servers/Main [1.16.5]/main_the_end/DIM1/region/' + deletefile)
+    #make sure to include a slash at the very end of the path for this one                                     ^^
 
     
