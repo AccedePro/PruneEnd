24b49ac5f9dc346ee7aeb0b5d7e5c07f63293466
Noah de Boers <63986217+AccedePro@users.noreply.github.com>
2021-04-28 03:22:10 +1000

    Update PruneEnd.bat

--- PruneEnd.bat
+++ PruneEnd.bat
@@ -1,5 +1,6 @@
 "C:\Users\WinServ\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\WinServ\Documents\IsisCraft (Bungeecord)\PruneEnd.py"
 
+::"C:\Users\******\AppData\Local\Programs\Python\******\python.exe" "C:\Users\******\Documents\******\PruneEnd.py"
 
 
 
